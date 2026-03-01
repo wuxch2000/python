a1dc108c6939d8bb3145f9a88918318f3a86eb2b
wuxch2000 <wuxch2000@gmail.com>
2026-02-28 18:25:23 -0800

    cipush

--- bouncing-ball/arcade-bouncing-ball.py
+++ bouncing-ball/arcade-bouncing-ball.py
@@ -13,9 +13,53 @@ class Data:
     bar_width     = 80
     bar_height    = 10
     bar_pos       = Pos(window_width/2, 50)
-    bar_move_speed = 5
+    bar_move_speed = 20
     def __init__(self):
         return
+
+class Bar(arcade.SpriteSolidColor):
+    def __init__(self):
+        super().__init__(data.bar_width, data.bar_height, arcade.color.YELLOW)
+        self.center_x, self.center_y = data.bar_pos.x, data.bar_pos.y
+        return
+    def update(self, delta_time: float = 1/60):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        return
+
+class BarList(arcade.SpriteList):
+    def __init__(self):
+        super().__init__()
+        self.left_pressed  = False
+        self.right_pressed = False
+        self.change_x = 0
+        return
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+        self._update_speed()
+        return
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
+        self._update_speed()
+        return
+    def _update_speed(self):
+        self.change_x = 0
+        if self.left_pressed and not self.right_pressed:
+            self.change_x = -data.bar_move_speed
+        elif self.right_pressed and not self.left_pressed:
+            self.change_x = data.bar_move_speed
+        return
+    def update(self, delta_time: float = 1/60):
+        super().move(self.change_x, 0)
+        super().update()
+        return
+
 
 class BouncingView(arcade.Window):
     def __init__(self):
@@ -24,9 +68,8 @@ class BouncingView(arcade.Window):
         return
     def setup(self):
         self.bar_texture = arcade.make_soft_square_texture(size=50, color=arcade.color.RED)
-        bar = arcade.SpriteSolidColor(data.bar_width, data.bar_height, arcade.color.RED)
-        bar.center_x, bar.center_y = data.bar_pos.x, data.bar_pos.y
-        self.bar_sprite_list = arcade.SpriteList()
+        bar = Bar()
+        self.bar_sprite_list = BarList()
         self.bar_sprite_list.append(bar)
         return
     def on_draw(self):
@@ -34,21 +77,16 @@ class BouncingView(arcade.Window):
         self.bar_sprite_list.draw()
         return
     def on_update(self, delta_time):
-        # Move the player
         self.bar_sprite_list.update(delta_time)
         return
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE or key == arcade.key.Q:
             self.close()
-        elif key == arcade.key.LEFT:
-            self.bar_sprite_list.change_x = -data.bar_move_speed
-        elif key == arcade.key.RIGHT:
-            self.bar_sprite_list.change_x = data.bar_move_speed
+        self.bar_sprite_list.on_key_press(key, modifiers)
         return
-
     def on_key_release(self, key, modifiers):
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.bar_sprite_list.change_x = 0
+        self.bar_sprite_list.on_key_release(key, modifiers)
+        return
 
 def main():
     global data
